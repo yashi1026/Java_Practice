5cdebdaf8a676366784a38d21d4cffc9b4fced0e
yashi1026 <zoneofyashi@gmail.com>
2025-08-11 20:47:54 +0530

    Add files via upload

---
+++ Array/ThreeSum.java
@@ -0,0 +1,15 @@
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        System.out.println(nums[i] + ", " + nums[j] + ", " + nums[k]);
+                    }
+                }
+            }
+        }
+    }
+}
+
