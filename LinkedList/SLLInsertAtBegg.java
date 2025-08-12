36c2f53d2d526156cc5ac2ae13f87b3fca2d7633
yashi1026 <zoneofyashi@gmail.com>
2025-08-12 22:00:21 +0530

    Add files via upload

---
+++ LinkedList/SLLInsertAtBegg.java
@@ -0,0 +1,28 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class SLLInsertAtBegg{
+    static Node insertAtBeginning(Node head, int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        return newNode;
+    }
+     public static void main(String[] args) {
+        Node head = null;
+        head = insertAtBeginning(head, 30);
+        head = insertAtBeginning(head, 20);
+        head = insertAtBeginning(head, 10);
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+}
+
