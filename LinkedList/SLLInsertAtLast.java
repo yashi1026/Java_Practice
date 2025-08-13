a38599719d036f083ebbb6282022295a62e2b051
yashi1026 <zoneofyashi@gmail.com>
2025-08-13 12:54:27 +0530

    Update SLLInsertAtLast.java

--- LinkedList/SLLInsertAtLast.java
+++ LinkedList/SLLInsertAtLast.java
@@ -10,8 +10,11 @@ class Node {
 public class SLLInsertAtLast {
     static Node insertAtEnd(Node head, int data) {
         Node newNode = new Node(data);
-        if (head == null) return newNode;
+        if (head == null) 
+        return newNode;
+        
         Node temp = head;
+        
         while (temp.next != null) {
             temp = temp.next;
         }
@@ -23,10 +26,13 @@ public class SLLInsertAtLast {
         head = insertAtEnd(head, 10);
         head = insertAtEnd(head, 20);
         head = insertAtEnd(head, 30);
+        
         Node current = head;
+        
         while (current != null) {
             System.out.print(current.data + " ");
             current = current.next;
         }
     }
 }
+
