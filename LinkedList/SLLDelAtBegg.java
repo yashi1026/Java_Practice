94b4ba99f928605c9e588f0e8c6ce2de58e63d85
yashi1026 <zoneofyashi@gmail.com>
2025-08-17 22:28:19 +0530

    Add files via upload

---
+++ LinkedList/SLLDelAtBegg.java
@@ -0,0 +1,24 @@
+public class SLLDelAtBegg {
+
+    static Node deleteAtBeginning(Node head) {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return null;
+        }
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+
+        head = deleteAtBeginning(head); 
+
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+}
