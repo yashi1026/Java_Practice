ef1723d8e4a61966ac61781ae18c4505969b778f
yashi1026 <zoneofyashi@gmail.com>
2025-09-10 15:05:38 +0530

    Add files via upload

---
+++ Tree/TreeHeight.java
@@ -0,0 +1,31 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+public class TreeHeight {
+
+    public int height(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+    public static void main(String[] args) {
+        
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        TreeHeight tree = new TreeHeight();
+        int treeHeight = tree.height(root);
+
+        
+        System.out.println("Height of the tree: " + treeHeight);
+    }
+}
